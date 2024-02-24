d1c715a45f0896a94b80adecd3fde8bef387552c
Tran Duy Phat <115158729+DuyPhatpeo@users.noreply.github.com>
2024-02-24 23:11:38 +0700

    Chuyen file

--- Java/TranDuyPhat-63135054/src/bt_java/bt4/MainBT4.java
+++ Java/TranDuyPhat-63135054/src/bt_java/bt4/MainBT4.java
@@ -7,9 +7,9 @@ public class MainBT4 {
         qlcx.themChuyenXe(new ChuyenXeNoiThanh("002", "Duc Phanh", "79A-999999", 100000,1,10));
         qlcx.themChuyenXe(new ChuyenXeNoiThanh("003", "Lam", "79A-888888", 100000,1,10));
         qlcx.themChuyenXe(new ChuyenXeNoiThanh("004", "Phuc Ngu", "79A-111111", 100000,1,10));
-        qlcx.themChuyenXe(new ChuyenXeNgoaiThanh("005", "Phuc Ngu", "79A-111111", 100000,"Nha Trang",2));
-        qlcx.themChuyenXe(new ChuyenXeNgoaiThanh("006", "Phuc Ngu", "79A-111111", 100000,"HCM",2));
-        qlcx.themChuyenXe(new ChuyenXeNgoaiThanh("007", "Phuc Ngu", "79A-111111", 100000,"Ha Noi",2));
+        qlcx.themChuyenXe(new ChuyenXeNgoaiThanh("005", "Phuc", "79A-111111", 100000,"Nha Trang",2));
+        qlcx.themChuyenXe(new ChuyenXeNgoaiThanh("006", "Ph", "79A-111111", 100000,"HCM",2));
+        qlcx.themChuyenXe(new ChuyenXeNgoaiThanh("007", "P", "79A-111111", 100000,"Ha Noi",2));
         qlcx.xuatDanhSach();
         System.out.println("Doanh thu xe noi thanh: " + qlcx.doanhThuXeNoiThanh());
         System.out.println("Doanh thu xe ngoai thanh: " + qlcx.doanhThuXeNgoaiThanh());
